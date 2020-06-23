000751e83d144de44a978ef9d08506b3394f9501
unicornaqq <xjwangqq@gmail.com>
2020-06-23 16:12:58 +0800

    submit a new version that works

--- median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays.py
@@ -1,58 +1,83 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1, nums2):
-        if len(nums1) != 1 or len(nums2) != 1:
-            
-            print(nums1)
-            if len(nums1) % 2 == 0:
-                # nums1 has even number of element
-                median_1 = (nums1[len(nums1)//2 - 1] + nums1[len(nums1)//2])/2
-                median_1_left = len(nums1)//2 - 1
-                median_1_right = len(nums1)//2
-            else:
-                # nums1 has odd number of element
-                median_1 = nums1[len(nums1) // 2]
-                if len(nums1) == 1:
-                    median_1_left = median_1_right = 0
-                else:
-                    median_1_left = len(nums1) // 2 - 1
-                    median_1_right = len(nums1) // 2 + 1
-            
-            print(nums2)
-            if len(nums2) % 2 == 0:
-                # nums2 has even number of element
-                median_2 = (nums2[len(nums2)//2 - 1] + nums2[len(nums2)//2])/2
-                median_2_left = len(nums2)//2 - 1
-                median_2_right = len(nums2)//2
-            else:
-                # nums1 has odd number of element
-                median_2 = nums2[len(nums2) // 2]
-                if len(nums2) == 1:
-                    median_2_left = median_2_right = 0
-                else:
-                    median_2_left = len(nums2) // 2 - 1
-                    median_2_right = len(nums2) // 2 + 1
-                
-            if median_1 > median_2:
-                print("case 1")
-                sub_nums1 = nums1[0:(median_1_left+1)]
-                sub_nums2 = nums2[median_2_right:]
-                print([median_1_left, median_2_right, sub_nums1, sub_nums2])
-                return self.findMedianSortedArrays(sub_nums1, sub_nums2)
-            elif median_1 < median_2:
-                print("case 2")
-                sub_nums1 = nums1[median_1_right:]
-                sub_nums2 = nums2[0:(median_2_left+1)]
-                print([median_1_right, median_2_left, sub_nums1, sub_nums2])
-                return self.findMedianSortedArrays(sub_nums1, sub_nums2)
-            else:
-                return median_1
-        else:
-            return (nums1[0] + nums2[0])/2
+import sys
 
-solution = Solution()
+class Solution:
+
+    def find_kth_element(self, nums1, nums2, begin1, begin2, k):
+        print(nums1[begin1:])
+        print(nums2[begin2:])
+        print(k)
+
+        """ if begin1 >= len(nums1):
+            # all elements of nums1 are kicked-out
+            return nums2[begin2 + k - 1] """
+        
+        if begin1 >= len(nums1):
+            # all elements of nums1 kicked out
+            # kth elements comes from nums2
+            return nums2[begin2 + k -1]
+        if begin2 >= len(nums2):
+            return nums1[begin1 + k - 1]
+        
+        if k == 1:
+            if nums1[begin1] < nums2[begin2]:
+                return nums1[begin1]
+            else:
+                return nums2[begin2]     
+
+            
+
+        if begin1 + k//2 > len(nums1):
+            # nums1 doesn't have so many elements
+            # get rid of other arrays k//2 elements
+            eleNum1 = sys.maxsize
+        else:
+            eleNum1 = nums1[begin1 + k//2 -1]
+        
+        if begin2 + k//2 > len(nums2):
+            eleNum2 = sys.maxsize
+        else:
+            eleNum2 = nums2[begin2 + k//2 - 1]
+
+        if eleNum1 > eleNum2:
+            # get rid of k//2 elements from nums2
+            begin1 = begin1
+            begin2 = begin2 + k//2
+            k = k - k//2
+            return self.find_kth_element(nums1, nums2, begin1, begin2, k)
+        else:
+            begin1 = begin1 + k//2
+            begin2 = begin2
+            k = k - k//2
+            return self.find_kth_element(nums1, nums2, begin1, begin2, k)
+
+
+    def findMedianSortedArrays(self, nums1, nums2):
+        left = (len(nums1) + len(nums2) + 1)//2
+        right = (len(nums1) + len(nums2) + 2)//2
+        if left == right:
+            return self.find_kth_element(nums1, nums2, 0, 0, left) 
+        else:
+            return (self.find_kth_element(nums1, nums2, 0, 0, left) + self.find_kth_element(nums1, nums2, 0, 0, right))/2
+
+
+""" solution = Solution()
 nums1 = [1, 2, 3, 5, 10]
 nums2 = [3, 4]
 result = solution.findMedianSortedArrays(nums1, nums2)
 print(result)
-assert result == 5
+assert result == 3
+
+nums1 = [-5, 1, 2,3]
+nums2 = [-1, 3]
+result = solution.findMedianSortedArrays(nums1, nums2)
+print(result)
+assert result == 1.5 """
+
+
+solution = Solution()
+nums1 = [-5, 1, 2,3, 7, 9]
+nums2 = [-80, -7]
+result = solution.findMedianSortedArrays(nums1, nums2)
+print(result)
+assert result == 1.5
 
