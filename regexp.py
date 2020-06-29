e9f64eaa0e456005eff605c21e810e89755d6dff
unicornaqq <xjwangqq@gmail.com>
2020-06-29 17:07:05 +0800

    enhanced for some case, but still have some issue

--- regexp.py
+++ regexp.py
@@ -9,9 +9,8 @@ class Solution:
             return False
         
         precedence = None
-        star_mode = False
         
-        for i in range(0, min(len(s), len(p))):
+        """ for i in range(0, min(len(s), len(p))):
             print("p[{}] is {}, and s[{}] is {}".format(i, p[i], i, s[i]))
             if p[i] == ".":
                 precedence = "."
@@ -33,11 +32,56 @@ class Solution:
             else:
                 return False
                 
-        return result
-                
-                
+        return result """
+        
+        (i, j) = (0, 0)        
+        while i < len(s) and j < len(p):
+            print("p[{}] is {}, and s[{}] is {}".format(j, p[j], i, s[i]))
+            if p[j] == ".":
+                precedence = "."
+                i+=1
+                j+=1
+                continue
+            elif p[j] == "*":
+                if precedence != None:
+                    if precedence == ".":
+                        """ not always true, for example:
+                        string "abc", pattern "a.*b"
+                        How to handle this case. """
+                        
+                        return True
+                    elif precedence == s[i]:
+                        i+=1
+                        continue
+                    else:
+                        j+=1
+                        continue
+                else:
+                    return False
+            elif p[j] == s[i]:
+                precedence = p[i]
+                i+=1
+                j+=1
+                continue
+            else:
+                return False
+        
+        return result           
                 
 to_match = "aab"
-pattern = "c*a*b"
 solution = Solution()
-print(solution.isMatch(to_match, pattern))
+test_map = dict([
+    ("a.*b", True),
+    ("a***b", True),
+    ("aab", True),
+    ("a.b", True),
+    ("c*a*b", True),
+    ("a.*c", False)
+])
+for (k, v) in test_map.items():
+    print(k)
+    try:
+        assert (solution.isMatch(to_match, k)) == v
+    except AssertionError as error:
+        print("case with pattern {} failed".format(k))
+    
