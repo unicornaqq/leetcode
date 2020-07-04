2f881d1813708955f506604b3f9abd099a80bbd9
unicornaqq <xjwangqq@gmail.com>
2020-07-04 22:07:05 +0800

    all the test case can pass, except the . case, need to think about how to handle it, go to sleep now, 10PM now

--- regexp.py
+++ regexp.py
@@ -5,7 +5,7 @@ import sys
 if len(sys.argv) == 4:
     (_, string, pattern, expected_result) = sys.argv[:]
 
-DEBUG = 1
+DEBUG = 0
 
 def log(var):
     if DEBUG == 1:
@@ -17,20 +17,29 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         subStringInP = p.split('.*')
         log("the len of the string is {}".format(len(s)))
+        log("Raw:{}".format(s))
+        log("Pattern:{}".format(p))
         log(subStringInP)
-        log(s)
+
 
         left_index = 0
+        right_index = 0
         offset = 0
+        roffset = 0
         
         for (index, sub) in enumerate(subStringInP):
             # with this method, we can make sure the sub
             # doesn't contain any .*, it means
             # it contains just char, . or *
             try:
-                left_index = s.index(sub, offset)
-                
-                offset = left_index + len(sub)
+                if sub == '':
+                    (left_index, right_index, offset, roffset) = (left_index, right_index, offset, roffset)
+                else:
+                    left_index = s.index(sub, offset)
+                    right_index = s.rfind(sub, offset)
+                    offset = left_index + len(sub)
+                    roffset = right_index + len(sub)
+                log([left_index, right_index])
             except ValueError:
                 # there could be * or . in the sub string
                 # try to split the sub string with * first
@@ -42,17 +51,20 @@ class Solution:
                 log(starSubString)
                 # return None
                 previous_star_char_index = 0
+                previous_star_char_rindex = 0
                 previous_star_char = ''
                 first_star_match = 0
                 star_offset = offset # we start from the end of last matched sub string
+                star_roffset = roffset
                 for (star_index, star_sub) in enumerate(starSubString):
-                    log([star_index, star_sub, star_offset])
+                    log([star_index, star_sub, star_offset, star_roffset])
                     if star_sub == '':
                         # this is corresponding to the case 
                         # with multiple * concatenated
                         continue
                     try:
                         star_left_index = s.index(star_sub, star_offset)
+                        star_right_index = s.rfind(star_sub, star_offset)
                         if first_star_match == 0:
                             # this is the first sub string of
                             # the string splited by *
@@ -62,6 +74,7 @@ class Solution:
                             # left_index
                             first_star_match = 1
                             left_index = star_left_index
+                            right_index = star_right_index
                         else:
                             # def, opl for the e.g. above
                             # check whether the * matches 0 char
@@ -70,14 +83,19 @@ class Solution:
                             # 0 char. don't need to care about in this 
                             # branch. And it is handled in the exception
                             # handling logic.
-                            if previous_star_char != '' and any([ c != previous_star_char for c in s[previous_star_char_index:star_left_index]]):
+                            if previous_star_char != '' and (
+                                any([ c != previous_star_char for c in s[previous_star_char_index:star_left_index]]) or 
+                                any([ c != previous_star_char for c in s[previous_star_char_rindex:star_right_index]])):
                                 return False 
-                        log(star_left_index)
-                        star_offset += len(star_sub)
+                        log([star_left_index, star_right_index])
+                        star_offset = star_left_index + len(star_sub)
+                        star_roffset = star_right_index + len(star_sub)
                         # the char to be repeated is at the end of substring
                         previous_star_char_index = star_offset - 1
+                        previous_star_char_rindex = star_roffset - 1
                         previous_star_char = star_sub[-1] # the char before * is the one to repeat
                         offset = star_offset # this is for the next iterate for .* splited substring
+                        roffset = star_roffset
                     except ValueError:
                         if star_sub[:-1] == '':
                             # such as a*
@@ -88,11 +106,15 @@ class Solution:
                                 # so need to get rid of the end char, and try
                                 # to match again
                                 star_left_index = s.index(star_sub[:-1], star_offset)
+                                star_right_index = s.rfind(star_sub[:-1], star_offset)
                                 if first_star_match == 0:
                                     first_star_match = 1
                                     left_index = star_left_index
+                                    right_index = star_right_index
                                 star_offset += len(star_sub[:-1])
+                                star_roffset += len(star_sub[:-1])
                                 offset = star_offset
+                                roffset = star_roffset
 
                             except ValueError:
                                 # it is possible, there is/are . inside the current substring
@@ -103,43 +125,46 @@ class Solution:
                                 else:
                                     # for (dot_index, dot_sub) in enumerate(star_sub):
                                     #     #TODO
-                                    return True
+                                    return False
             if index == 0:
                 # it is the head
-                log("it is the head")
-                if left_index != 0:
+                log("it is the head offset={}, roffset={}".format(offset, roffset))
+                if left_index != 0 or (sub.find("*") != -1 and offset != roffset):
                     # it doesn't match the head of the string
                     # although it should do. Return 'False' 
                     # directly.
                     return False
             elif index == len(subStringInP) - 1:
-                log("it is the tail")
-                if offset != len(s):
+                log("it is the tail offset={}, roffset={}".format(offset, roffset))
+                if offset != len(s) and roffset != len(s):
                     return False
             else:
                 log("it is the middle")
-                if left_index == 0 or offset == len(s):
-                    return False
+                # if left_index == 0 or offset == len(s):
+                #     return False
                 
         # subStringInP = p.split('.')        
-
-        return True
+        if offset != len(s) and roffset != len(s):
+            return False
+        else:
+            return True
 
 
 solution = Solution()
-print(solution.isMatch("afafbeeab", "a.*b"))
-# for the case below, you can't match the b  after * to the
-# final b. so how to decide which b should be matched?
-print(solution.isMatch("afafbeeab", "a.*beeab"))
-# same case, if we start from back to the front, which a 
-# should be matched to the a within (a.*)?
-print(solution.isMatch("afafbeeab", "afa.*beeab"))
-print(solution.isMatch("afafbeeab", "afa.*beeab"))
-print(solution.isMatch("afafbeeabxxbeeab", "afa.*beeab"))
-# the beeab definitely should match the last beeab in
-# the origianl string
-print(solution.isMatch("afafbeeabxxbeeab", "afa.*beeab.*b"))
-print(solution.isMatch("afafbeeabxxbeeab", "ada.*beeab.*b"))
+# print(solution.isMatch("aaaab", "a*b")) # the only match case: aaaab vs a*b
+# print(solution.isMatch("afafbeeab", "a.*b"))
+# # for the case below, you can't match the b  after * to the
+# # final b. so how to decide which b should be matched?
+# print(solution.isMatch("afafbeeab", "a.*beeab"))
+# # same case, if we start from back to the front, which a 
+# # should be matched to the a within (a.*)?
+# print(solution.isMatch("afafbeeab", "afa.*beeab"))
+# print(solution.isMatch("afafbeeab", "afa.*beeab"))
+# print(solution.isMatch("afafbeeabxxbeeab", "afa.*beeab"))
+# # the beeab definitely should match the last beeab in
+# # the origianl string
+# print(solution.isMatch("afafbeeabxxbeeab", "afa.*beeab.*b"))
+# print(solution.isMatch("afafbeeabxxbeeab", "ada.*beeab.*b"))
 
 if expected_result == None:                
     to_match = "aab"
