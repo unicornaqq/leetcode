76b4e15b34e265c55cc58f673b34d1c8b64edc30
unicornaqq <xjwangqq@gmail.com>
2020-08-28 11:04:57 +0800

    solution

--- longestValidParentheses.py
+++ longestValidParentheses.py
@@ -20,22 +20,39 @@ hit )
     else, pop it, and update the length +=2
         if the stack is empty, remember the length as previous, and update the max_len, cumulative = 0
         else, update the max_len
-    
-级联的case
-（）（） 2
-（（）） 1
-（（（）（））） 2 1 1
-）（）（））
-（（）））））（（（）（）（）（）））））（（（
-遇到（就push
-遇到）
-	stack 非空，就pop，当前长度=2，如果和前面的级联，就计算新的长度，并更新最长， update prev 的index
-	stack空，prev_index == 0, 继续扫描
 
+write down some cascade case, and figure out the pattern, so helps me to get the logic:
+
+                    if (index - prev_index) < 2: # for local case
+                        current_len += prev_len
+
+And we need to take into the consideration the case that we need to connect the local longest string with a remote long string
+when poping out the (, this helps to connect to remote long string.
 
  """
-from stack import Stack
-from stack import StackElement
+class StackElement:
+    def __init__(self, value, next=None):
+        self.val = value
+        self.next = next
+
+class Stack:
+    def __init__(self):
+        self.top = None
+    def push(self, item:StackElement):
+        if self.top == None:
+            self.top = item
+        else:
+            item.next = self.top
+            self.top = item
+    def pop(self):
+        if self.top == None:
+            return self.top
+        else:
+            temp = self.top
+            self.top = temp.next
+            return temp
+    def empty(self):
+        return self.top == None
 
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
@@ -45,21 +62,28 @@ class Solution:
         max_len = 0
         max_index = 0
         prev_len = 0
+        push_prev_len = False
         for (index, bracket) in enumerate(s):
             if bracket == '(':
-                ele = StackElement('(')
+                if push_prev_len == True:
+                    ele = StackElement(prev_len)
+                    push_prev_len = False
+                else:
+                    ele = StackElement(0)
                 temp_stack.push(ele)
             else:
                 if not temp_stack.empty():
-                    temp_stack.pop()
+                    ele = temp_stack.pop()
                     current_len = 2
-                    if (index - prev_index) < 3:
+                    if (index - prev_index) < 2: # for local case
                         current_len += prev_len
+                    current_len += ele.val # for the case that need to connect with 
                     prev_index = index
                     prev_len = current_len
                     if current_len > max_len:
                         max_len = current_len
                         max_index = index
+                    push_prev_len = True
                 else:
                     prev_index = 0
                     prev_len = 0
@@ -68,15 +92,16 @@ class Solution:
     
 sol = Solution()
 s = ")()())"
-s = "()"
-s = ""
-s = ")"
-s = "("
+# s = "()"
+# s = ""
+# s = ")"
+# s = "("
 # s = "(()"
 # s = "()(((((()"
 # s = "()(((((())"
-s = "((())))()(()())()()()()(((())))))(()()()()()()((((((())))"
-s = "()(()())"
+# s = "((())))()(()())()()()()(((())))))(()()()()()()((((((())))"
+# s = "()(()())"
+s = "((((((())))(((((())))))(())((((())))()((((((((((((((())()))))))(()))()()(((((((())))))))))(((((()))))))(((((((((((((((((()))))))))))))))))))))))))))))))))))((()))())())())()))))))))))))))"
 print(sol.longestValidParentheses(s))
 
 # the current solution can't handle the case as : ()(()())
