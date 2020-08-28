04ecd7b83b07312498f3de53dae20c47eaef2839
unicornaqq <xjwangqq@gmail.com>
2020-08-28 11:40:53 +0800

    always use a list to mimic the behavior of a stack or queue

--- longestValidParentheses.py
+++ longestValidParentheses.py
@@ -39,34 +39,11 @@ import atexit
 profile = line_profiler.LineProfiler()
 atexit.register(profile.print_stats)
 
-class StackElement:
-    def __init__(self, value, next=None):
-        self.val = value
-        self.next = next
-
-class Stack:
-    def __init__(self):
-        self.top = None
-    def push(self, item:StackElement):
-        if self.top == None:
-            self.top = item
-        else:
-            item.next = self.top
-            self.top = item
-    def pop(self):
-        if self.top == None:
-            return self.top
-        else:
-            temp = self.top
-            self.top = temp.next
-            return temp
-    def empty(self):
-        return self.top == None
-
 class Solution:
     @profile
     def longestValidParentheses(self, s: str) -> int:
-        temp_stack = Stack()
+        # temp_stack = Stack()
+        temp_stack = []
         prev_index = 0
         current_len = 0
         max_len = 0
@@ -76,18 +53,17 @@ class Solution:
         for (index, bracket) in enumerate(s):
             if bracket == '(':
                 if push_prev_len == True:
-                    ele = StackElement(prev_len)
+                    temp_stack.append(prev_len)
                     push_prev_len = False
                 else:
-                    ele = StackElement(0)
-                temp_stack.push(ele)
+                    ele = temp_stack.append(0)
             else:
-                if not temp_stack.empty():
+                if not len(temp_stack) == 0:
                     ele = temp_stack.pop()
                     current_len = 2
                     if (index - prev_index) < 2: # for local case
                         current_len += prev_len
-                    current_len += ele.val # for the case that need to connect with 
+                    current_len += ele # for the case that need to connect with 
                     prev_index = index
                     prev_len = current_len
                     if current_len > max_len:
@@ -109,7 +85,7 @@ s = ")()())"
 # s = "(()"
 # s = "()(((((()"
 # s = "()(((((())"
-# s = "((())))()(()())()()()()(((())))))(()()()()()()((((((())))"
+s = "((())))()(()())()()()()(((())))))(()()()()()()((((((())))"
 # s = "()(()())"
-s = "((((((())))(((((())))))(())((((())))()((((((((()()()(((())()()(((((()))))()()())())))))((((()()()))((((((()))())(((((((())()))))))(()))()()(((((((())))))))))(((((()))))))(((((((((((((((((()))))))))))))))))))))))))))))))))))((()))())())())()))))))))))))))"
+# s = "((((((())))(((((())))))(())((((())))()((((((((()()()(((())()()(((((()))))()()())())))))((((()()()))((((((()))())(((((((())()))))))(()))()()(((((((())))))))))(((((()))))))(((((((((((((((((()))))))))))))))))))))))))))))))))))((()))())())())()))))))))))))))"
 print(sol.longestValidParentheses(s))
