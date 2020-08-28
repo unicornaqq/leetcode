3c130d94ee17e3022fe7a5f68ba41e3954a6d0ff
unicornaqq <xjwangqq@gmail.com>
2020-08-28 18:12:11 +0800

    init version

--- isValidSudoku.py
+++ isValidSudoku.py
@@ -45,8 +45,28 @@ Note:
 A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 Only the filled cells need to be validated according to the mentioned rules.
 The given board contain only digits 1-9 and the character '.'.
-The given board size is always 9x9. """
+The given board size is always 9x9. 
 
+Need a command function to check if upto 9 digits are unique.
 
+"""
+
+import numpy as np
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+    def isValidSudoku(self, board) -> bool:
+        a = np.array(board)
+        print(a)
+
+sol = Solution()
+board = [
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+print(sol.isValidSudoku(board))
